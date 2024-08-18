ccafba3a8f356fcdd22b85111e687285f3d30144
lee minseok <lms31782@gmail.com>
2024-08-18 16:27:31 +0900

    삽입정렬 추가

---
+++ __test__/sorting/insertionSort.test.ts
@@ -0,0 +1,33 @@
+import { insertionSort } from '../../src/sorting/insertionSort';
+import { generateArray } from '../../src/utils';
+
+describe('삽입 정렬', () => {
+  it('기본적인 정렬', () => {
+    const unsortedArray = [64, 25, 12, 22, 11];
+    const sortedArray = insertionSort(unsortedArray);
+    expect(sortedArray).toEqual([11, 12, 22, 25, 64]);
+  });
+  it('이미 정렬된 배열 정렬', () => {
+    const unsortedArray = [11, 12, 22, 25, 64];
+    const sortedArray = insertionSort(unsortedArray);
+    expect(sortedArray).toEqual([11, 12, 22, 25, 64]);
+  });
+  it('중복된 요소를 가진 배열 정렬', () => {
+    const unsortedArray = [11, 12, 22, 25, 64];
+    const sortedArray = insertionSort(unsortedArray);
+    expect(sortedArray).toEqual([11, 12, 22, 25, 64]);
+  });
+  it('빈 배열 정렬', () => {
+    const unsortedArray: number[] = [];
+    const sortedArray = insertionSort(unsortedArray);
+    expect(sortedArray).toEqual([]);
+  });
+  it('10,000개의 요소를 가진 배열을 1초 이내에 정렬', () => {
+    const unsortedArray = generateArray(10000);
+    const startTime = performance.now();
+    insertionSort(unsortedArray);
+    const endTime = performance.now();
+
+    expect(endTime - startTime).toBeLessThan(1000);
+  });
+});
--- src/sorting/insertionSort.ts
+++ src/sorting/insertionSort.ts
@@ -1,1 +1,18 @@
-// 삽입정렬
+// 삽입 정렬
+export const insertionSort = (arr: number[]) => {
+  const n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    // 현재 요소(key)를 정렬된 부분(arr[0] ... arr[i-1])과 비교하여 삽입할 위치를 찾습니다.
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key; // 올바른 위치에 key를 삽입
+  }
+
+  return arr;
+};
